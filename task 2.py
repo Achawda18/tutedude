0dbd141dcd6590c645f38117c6f754a6ddeca113
achawda18 <chawdaavani166@gmail.com>
2026-02-21 18:58:44 +0530

    task2

--- task 2.py
+++ task 2.py
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
 total = 0
 for i in range(1,51):
     total=total+i
-=======
 total = 0
 for i in range(1,51):
     total=total+i
->>>>>>> f0273221f71bd3396c4832ba5938a9056696ce23
 print("the sum of integers from 1 to 50 is :",total)
---
+++ task2A3.py
@@ -0,0 +1,11 @@
+import math
+
+number = float(input("Enter a number: "))
+
+square_root = math.sqrt(number)
+natural_log = math.log(number)
+sine_value = math.sin(number)
+
+print("Square root:", square_root)
+print("Natural logarithm:", natural_log)
+print("Sine:", sine_value)
